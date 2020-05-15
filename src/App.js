4dd9428559a18fe2b53ba10636de6c6b90ed4e0c
Ali Almohaya <alialmohaya@gmail.com>
2020-05-15 23:57:13 +0200

    Add the basic app structre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path='/' render={() => (<div>This is the /</div>)} />
+
+        <Route path='/home' render={() => (<div>This is the home</div>)} />
+
+        <Route path='/login' render={() => (<div>This is the login</div>)} />
+
+        <Route path='/signup' render={() => (<div>This is the signup</div>)} />
+
+        <Route path='/logout' render={() => (<div>This is the logout</div>)} />
+      </div>
+    </BrowserRouter>
   );
 }
 
