6fe28dc4005281503e3f01cdc98a80ff411b04c5
Ali Almohaya <alialmohaya@gmail.com>
2020-05-16 18:20:46 +0200

    Add scss dirs structure and import to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie'
 
-import './index.css';
+import './styles/main.scss';
 
 import App from './components/App';
 
